8da222815ad1e408a4caec93f8f22afaf8789e44
AlvinBirley <alvinbirley@gmail.com>
2024-11-17 11:44:38 -0600

    Make div squares object and playerOne object

---
+++ script.js
@@ -0,0 +1,23 @@
+//object to create the game board 
+function gameBoard(){
+return {
+    board:[ null, null, null,
+            null, null, null,
+        "test", null, null
+    ]
+}
+}
+
+//player one object 
+function playerOne(name) {
+    //player One marker
+    const markerOne = "X"
+
+    return {name, markerOne}
+
+}
+
+ const Mark = playerOne("Mark Csernus");
+
+
+
